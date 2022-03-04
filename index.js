14f894a53e29baa72ae4f99b333f33471fcc0407
root <saung.myat@gmail.com>
2022-03-03 23:40:45 -0500

    commit tree 57116f5d0a4a45ac981ed6bcb4ff8caa45fb4b3d

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+const arr = [];
+
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        arr.push("Thank you, " + name[i] + ", for the wonderful " + event + " gift!");
+        debugger;
+    }
+        return arr;
+    
+}
+
+
+function countDown(count) {
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+    return count;
+
+}
